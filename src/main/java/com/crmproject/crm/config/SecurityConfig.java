29db0ab0eaeb7a77bee082c7afe9884c851b0fe3
Tamersa36 <tamer8@live.com>
2024-07-04 17:32:47 +0300

    remove security

--- src/main/java/com/crmproject/crm/config/SecurityConfig.java
+++ src/main/java/com/crmproject/crm/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-package com.crmproject.crm.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll() // Allow all requests without authentication
-                )
-                .csrf(csrf -> csrf.disable()); // Disable CSRF protection
-        return http.build();
-    }
-}
+//package com.crmproject.crm.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authz -> authz
+//                        .anyRequest().permitAll() // Allow all requests without authentication
+//                )
+//                .csrf(csrf -> csrf.disable()); // Disable CSRF protection
+//        return http.build();
+//    }
+//}
